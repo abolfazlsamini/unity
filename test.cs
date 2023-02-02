aebe20e95f06c7156803303e26f0dce6bba917d8
saminimaster <samini7a@gmail.com>
2023-02-03 00:19:11 +0330

    commit

--- test.cs
+++ test.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    // why it not work when you change it to an even number inside unity?
+    public int velos = 0;
+    int x = 1;
+    
     void Update()
     {
         
+        x = velos *x;
+        transform.rotation = Quaternion.Euler( new Vector3(x,0,0));
+    
     }
 }
